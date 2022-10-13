c177172871d8ddc5caa770cbbab1487f0f4994de
hongjunpyo11 <jpyo98@naver.com>
2022-10-14 02:57:09 +0900

    결과 조회 API, 기본 문법과 쿼리 API 마무리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,9 +19,12 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class) // 파라미터 바인딩 - 이름 기준
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+            System.out.println("singleResult = " + result.getUsername());
+
 
             tx.commit();
         } catch (Exception e) {
@@ -47,6 +51,12 @@ public class JpaMain {
  *   * TypeQuery: 반환 타입이 명확할 때 사용
  *   * Query: 반환 타입이 명확하지 않을 때 사용
  *
+ * 결과 조회 API
+ * query.getResultList(): 결과가 하나 이상일 때, 리스트 반환
+ *   * 결과가 없으면 빈 리스트 반환
+ * query.getSingleResult(): 결과가 정확히 하나, 단일 객체 반환
+ *   * 결과가 없으면: javax.persistence.NoResultException
+ *   * 둘 이상이면: javax.persistence.NonUniqueResultException
  */
 
 
