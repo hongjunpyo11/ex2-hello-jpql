5c006a811c53002860f6b42cac2b2f9818f0fdab
hongjunpyo11 <jpyo98@naver.com>
2022-10-15 00:14:27 +0900

    페이징

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,21 +14,25 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
 
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
-
+            System.out.println("result.size = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
@@ -43,14 +47,10 @@ public class JpaMain {
 }
 
 /**
- * 프로젝션
- *   * SELECT 절에 조회할 대상을 지정하는 것
- *   * 프로젝션 대상: 엔티티, 임베디드 타입, 스칼라 타입(숫자, 문자등 기본 데이터 타입)
- *   * SELECT m.FROM Member m -> 엔티티 프로젝션
- *   * SELECT m.team FROM Member m -> 엔티티 프로젝션
- *   * SELECT m.address FROM Member m -> 임베디드 타입 프로젝션
- *   * SELECT m.username, m.age FROM Member m -> 스칼라 타입 프로젝션
- *   * DISTINCT로 중복 제거
+ * 페이징 API
+ * * JPA는 페이징을 다음 두 API로 추상화
+ * * setFirstResult(int startPosition): 조회 시작 위치(0부터 시작)
+ * * setMaxResults(int maxResult): 조회할 데이터 수
  */
 
 
