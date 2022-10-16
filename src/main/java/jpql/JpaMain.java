758397e18c145390a63d3e6c8cfdf3bae786999a
hongjunpyo11 <jpyo98@naver.com>
2022-10-17 00:59:40 +0900

    JPQL 함수

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,51 +14,27 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("꽌리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
-            member.setTeam(team);
 
-            em.persist(member);
 
             em.flush();
             em.clear();
 
-            String query = "select nullif(m.username, '관리자') as username " +
-                    "from Member m ";
+            String query = "select function('group_concat', m.username) From Member m";
+
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
+
             for (String s : result) {
                 System.out.println("s = " + s);
-            } // COALESCE
-
-//            String query = "select coalesce(m.username, '이름 없는 회원') as username " +
-//                    "from Member m ";
-//            List<String> result = em.createQuery(query, String.class)
-//                    .getResultList();
-//            for (String s : result) {
-//                System.out.println("s = " + s);
-//            } // COALESCE
-
-//            String query =
-//                    "select " +
-//                            "case when m.age <= 10 then '학생요금' " +
-//                            "     when m.age >= 60 then '경로요금' " +
-//                            "     else '일반요금' " +
-//                            "end " +
-//                    "from Member m";
-//            List<String> result = em.createQuery(query, String.class)
-//                    .getResultList();
-//
-//            for (String s : result) {
-//                System.out.println("s = " + s);
-//            } 기본 CASE 식
+            }
 
             tx.commit();
         } catch (Exception e) {
@@ -72,30 +48,20 @@ public class JpaMain {
     }
 }
 /**
- * 조건식 - CASE 식
- *   * 기본 CASE 식
- *     * select
- *         case when m.age <= 10 then '학생요금'
- *              when m.age >= 60 then '경로요금'
- *              else '일반요금'
- *         end
- *       from Member m
+ * JPQL 기본 함수
+ *   * CONCAT
+ *   * SUBSTRING
+ *   * TRIM
+ *   * LOWER, UPER
+ *   * LENGTH
+ *   * LOCATE
+ *   * ABS, SQRT, MOD
+ *   * SIZE, INDEX(JPA 용도)
  *
- *   * 단순 CASE 식
- *     * select
- *         case when '팀A' then '인센티브 110%'
- *              when '팀B' then '인센티브 120%'
- *              else '인센티브 105%'
- *         end
- *       from Team t
- *
- *   * COALESCE: 하나씩 조회해서 null이 아니면 반환
- *     * 사용자 이름이 없으면 이름 없는 회원을 반환
- *       * select coalesce(m.username, '이름 없는 회원') from Member m
- *
- *   * NULLIF: 두 값이 같으면 null 반환, 다르면 첫번째 값 반환
- *     * 사용자 이름이 '관리자'면 null 을 반환하고 나머지는 본인의 이름을 반환
- *       * select NULLIF(m.username, '관리자') from Member m
+ * 사용자 정의 함수 호출
+ *   * 하이버네이트는 사용전 방언에 추가해야 한다.
+ *     * 사용하는 DB방언을 상속받고, 사용자 정의 함수를 등록한다.
+ *     select function('group_concat', i.name) from Item i
  */
 
 
