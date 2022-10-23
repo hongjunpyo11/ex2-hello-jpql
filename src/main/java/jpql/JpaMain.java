acbca11ee0c508166bb1e63e3ca614756b594176
hongjunpyo11 <jpyo98@naver.com>
2022-10-23 18:43:24 +0900

    페치 조인 2 - 한계

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -41,17 +41,21 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t join fetch t.members";
+//            String query = "select m From Member m join fetch m.team t"; 방향을 바꿔서 페이징 해결
 
-            List<Team> result = em.createQuery(query, Team.class)
+            String query = "select t From Team t";
+
+            List<Team> result = em.createQuery(query, Team.class) // <property name="hibernate.default_batch_fetch_size" value="100"/>를 사용해 n+1 문제 해결
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
             System.out.println("result = " + result.size());
 
             for (Team team : result) {
-                System.out.println("team = " + team.getName() + "|members=" + team.getMembers());
-                for (Team team1 : result) {
-
+                System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
                 }
             }
 
@@ -68,49 +72,25 @@ public class JpaMain {
     }
 }
 /**
- * 페치 조인(fetch join)
- *   * SQL 조인 종류 X
- *   * JPQL에서 성능 최적화를 위해 제공하는 기능
- *   * 연관된 엔티티나 컬렉션을 SQL 한 번에 함께 조회하는 기능
- *   * join fetch 명령어 사용
- *   * 페치 조인 ::= [LEFT [OUTER] | INNER] JOIN FETCH 조인경로
+ * 페치 조인의 특성과 한계 (페치 조인은 너무너무 중요하고 페치조인에 대해서는 100프로 이해를 해야함)
+ *   * 페치 조인 대상에는 별칭을 줄 수 없다.
+ *     * 하이버네이트는 가능, 가급적 사용 X
+ *   * 둘 이상의 컬렉션은 페치 조인 할 수 없다.
+ *   * 컬렉션을 페치 조인하면 페이징 API(setFirstResult, setMaxResult)를 사용할 수 없다.
+ *     * 일대일, 다대일 같은 단일 값 관련 연관 필드들은 페치 조인해도 페이징 가능
+ *     * 하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)
  *
- * 엔티티 페치 조인
- *   * 회원을 조회하면서 연관된 팀도 함께 조회(SQL 한 번에)
- *   * SQL을 보면 회원 뿐만 아니라 팀(T.*)도 함께 SELECT
- *   * [JPQL]
- *     select m from Member m join fetch m.team
- *   * [SQL]
- *     SELECT M.*, T.* FROM MEMBER M
- *     INNER JOIN TEAM T ON M.TEAM_ID = T.ID
+ * 페치 조인의 특징과 한계
+ *   * 연관된 엔티티들을 SQL 한 번으로 조회 - 성능 최적화
+ *   * 엔티티에 직접 적용하는 글로벌 로딩 전략보다 우선함
+ *     * OneToMany(fetch = FetchType.LAZY) // 글로벌 로딩 전략
+ *   * 실무에서 글로벌 로딩 전략은 모두 지연 로딩
+ *   * 최적화가 필요한 곳은 페치 조인 적용
  *
- * 컬렉션 페치 조인
- *   * 일대다 관계, 컬렉션 페치 조인
- *
- *   * [JPQL]
- *     select t
- *     from Team t join fetch t.members
- *     where t.name = '팀A'
- *
- *   * [SQL]
- *     SELECT T.*, M.*
- *     FROM TEAM T
- *     INNER JOIN MEMBER M ON T.ID = M.TEAM_ID
- *     WHERE T.NAME = '팀A'
- *
- * 페치 조인과 DISTINCT
- *   * SQL의 DISTINCT는 중복된 결과를 제거하는 명령
- *   * JPQL의 DISTINCT 2가지 기능 제공
- *     * 1. SQL에 DISTINCT를 추가
- *     * 2. 애플리케이션에서 엔티티 중복 제거
- *
- * 페치 조인과 일반 조인의 차이
- *   * 일반 조인 실행시 엔티티를 함께 조회하지 않음
- *   * JPQL은 결과를 반환할 때 연관관계 고려X
- *   * 단지 SELECT 절에 지정한 엔티티만 조회할 뿐
- *   * 여기서는 팀 엔티티만 조회하고, 회원 엔티티는 조회X
- *   * 페치 조인을 사용할 때만 연관된 엔티티도 함께 조회(즉시 로딩)
- *   * 페치 조인은 객체 그래프를 SQL 한번에 조회하는 개념
- */
-
+ * 페치 조인 - 정리
+ *   * 모든 것을 페치 조인으로 해결할 수는 없음
+ *   * 페치 조인은 객체 그래프를 유지할 때 사용하면 효과적
+ *   * 여러 테이블을 조인해서 엔티티가 가진 모양이 아닌 전혀 다른 결과를 내야하면,
+ *     페치 조인 보다는 일반 조인을 사용하고 필요한 데이터들만 조회해서 DTO 로 반환 하는 것이 효과적
+ /
 
