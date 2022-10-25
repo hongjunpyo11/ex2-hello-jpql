7f7b4ee325ac55a215ed99a98a66106bfaeb63fb
hongjunpyo11 <jpyo98@naver.com>
2022-10-25 23:21:21 +0900

    다형성 쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -41,11 +41,9 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-//            String query = "select m From Member m join fetch m.team t"; 방향을 바꿔서 페이징 해결
-
             String query = "select t From Team t";
 
-            List<Team> result = em.createQuery(query, Team.class) // <property name="hibernate.default_batch_fetch_size" value="100"/>를 사용해 n+1 문제 해결
+            List<Team> result = em.createQuery(query, Team.class)
                     .setFirstResult(0)
                     .setMaxResults(2)
                     .getResultList();
@@ -71,26 +69,13 @@ public class JpaMain {
         emf.close();
     }
 }
-/**
- * 페치 조인의 특성과 한계 (페치 조인은 너무너무 중요하고 페치조인에 대해서는 100프로 이해를 해야함)
- *   * 페치 조인 대상에는 별칭을 줄 수 없다.
- *     * 하이버네이트는 가능, 가급적 사용 X
- *   * 둘 이상의 컬렉션은 페치 조인 할 수 없다.
- *   * 컬렉션을 페치 조인하면 페이징 API(setFirstResult, setMaxResult)를 사용할 수 없다.
- *     * 일대일, 다대일 같은 단일 값 관련 연관 필드들은 페치 조인해도 페이징 가능
- *     * 하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)
- *
- * 페치 조인의 특징과 한계
- *   * 연관된 엔티티들을 SQL 한 번으로 조회 - 성능 최적화
- *   * 엔티티에 직접 적용하는 글로벌 로딩 전략보다 우선함
- *     * OneToMany(fetch = FetchType.LAZY) // 글로벌 로딩 전략
- *   * 실무에서 글로벌 로딩 전략은 모두 지연 로딩
- *   * 최적화가 필요한 곳은 페치 조인 적용
- *
- * 페치 조인 - 정리
- *   * 모든 것을 페치 조인으로 해결할 수는 없음
- *   * 페치 조인은 객체 그래프를 유지할 때 사용하면 효과적
- *   * 여러 테이블을 조인해서 엔티티가 가진 모양이 아닌 전혀 다른 결과를 내야하면,
- *     페치 조인 보다는 일반 조인을 사용하고 필요한 데이터들만 조회해서 DTO 로 반환 하는 것이 효과적
- /
 
+/**
+ * 다형성 쿼리
+ *   * 조회 대상을 특정 자식으로 한정
+ *
+ *   * TREAT(JPA 2.1)
+ *     * 자바의 타입 캐스팅과 유사
+ *     * 상속 구조에서 부모 타입을 특정 자식 타입으로 다룰 때 사용
+ *     * FROM, WHERE, SELECT(하이버네이트 지원) 사용용
+ */
