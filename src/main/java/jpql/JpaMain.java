5409de75eb679fb8e7e4c9a579852efe00612a67
hongjunpyo11 <jpyo98@naver.com>
2022-10-27 16:22:48 +0900

    벌크 연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -24,30 +24,32 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
+            // FLUSH 자동 호출
+           int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
 
-            em.flush();
-            em.clear();
+           em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            Member findMember = em.find(Member.class, member1.getId());
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            System.out.println("findMember = " + findMember.getAge());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -61,24 +63,22 @@ public class JpaMain {
 }
 
 /**
- * Named 쿼리 - 어노테이션 너무 지져분해져서 좀 안좋다고 봄
- * @Entity
- * @NamedQurey(
- *        name = "Member.findByUsername",
- *        query = "select m from Member m where m.username = :username")
- * public class Member {
- *     ...
- * }
+ * 벌크연산
+ *   * 재고가 10개 미만인 모든 상품의 가격을 10% 상승하려면?
+ *   * JPA 변경 감지 기능으로 실행하려면 너무 많은 SQL 실행
+ *     * 재고가 10개 미만인 상품 리스트로 조회한다.
+ *     * 상품 엔티티의 가격을 10% 증가한다.
+ *     * 트랜잭션 커밋 시점에 변경감지가 동작한다.
+ *   * 변경된 데이터가 100건이라면 100번의 UPDATE SQL 실행
  *
- * List<Member> resultList =
- *   em.createNamedQuery("Member.findByUsername", Member.class)
- *          .setParameter("username", "회원1")
- *          .getResultList();
+ * 벌크 연산 예제
+ *   * 쿼리 한번으로 여러 테이블 로우 변경(엔티티)
+ *   * executeUpdate()의 결과는 영향받은 엔티티 수 반환
+ *   * UPDATE, DELETE 지원
+ *   * INSERT(insert into .. select, 하이버네이트 지원원)
  *
- * Named 쿼리 - 정적 쿼리
- *   * 미리 정의해서 이름을 부여해두고 사용하는 JQPL
- *   * 정적 쿼리
- *   * 어노테이션, XML에 정의
- *   * 애플리케이션 로딩 시점에 초기화 후 재사용
- *   * 애플리케이션 로딩 시점에 쿼리를 검증
- */
+ * 벌크 연산 주의
+ *   * 벌크 연산은 영속성 컨텍스트를 무시하고 데이터베이스에 직접 쿼리
+ *     * 벌크 연산을 먼저 실행
+ *     * 벌크 연산 수행 후 영속성 컨텍스트 초기화화
+* */
